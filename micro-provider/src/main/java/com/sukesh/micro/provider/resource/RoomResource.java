b03cbdc039f3f6b893575fcbf27b172b93a771bb
sukeshrs <sukeshrs@sukeshs-macbook-pro.local>
2017-12-26 21:53:58 -0500

    Added a POST resource

--- micro-provider/src/main/java/com/sukesh/micro/provider/resource/RoomResource.java
+++ micro-provider/src/main/java/com/sukesh/micro/provider/resource/RoomResource.java
@@ -3,7 +3,9 @@ package com.sukesh.micro.provider.resource;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -32,6 +34,19 @@ public class RoomResource {
 		
 		return allRooms;
 		
+	}
+	
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public List<Room> addNewRooms(Room room) {
+		List<Room> allRooms = new ArrayList<Room>();
+		allRooms.add(new Room(101,1));
+		allRooms.add(new Room(102,1));
+		allRooms.add(new Room(103,1));
+		allRooms.add(new Room(201,2));
+		allRooms.add(room);
+		return allRooms;
 	}
 	
 }
